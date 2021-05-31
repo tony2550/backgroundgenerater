6e5a80c741e5febcd09b9b3baa9b2302311c0555
Mookie Um <tony7320@gmail.com>
2021-05-31 15:17:09 +0900

    <first commit>

---
+++ app.js
@@ -0,0 +1,28 @@
+// alert('테스트')
+
+// 사용할 태그요소 객체를 변수에 저장한다.
+
+var body = document.getElementById('gradient');
+var color1 = document.querySelector('.color1');
+var color2 = document.querySelector('.color2');
+var h3 = document.querySelector('h3');
+
+// 이벤트를 연결한다
+
+// color1.addEventListener('input',function(){
+//     body.style.background = "linear-gradient(to right," + color1.value +", " + color2.value + ")";
+//     h3.textContent = body.style.background;
+// });
+
+// color2.addEventListener('input',function(){
+//     body.style.background = "linear-gradient(to right," + color1.value +", " + color2.value + ")";
+//     h3.textContent = body.style.background;
+// });
+
+color1.addEventListener('input' , grad);
+color2.addEventListener('input' , grad);
+
+function grad(){
+    body.style.background = "linear-gradient(to right," + color1.value +", " + color2.value + ")";
+    h3.textContent = body.style.background;
+};
